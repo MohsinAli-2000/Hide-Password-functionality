824145d1bbad68e6ad4261aae6c21bfff407ba15
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2023-10-24 13:19:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let password = document.getElementById("password");
 eyeIcon.onclick = ()=>{
     if (password.type == "password"){
         password.type = "text"
-        eyeIcon.src = "./eye-open.png"
+        eyeIcon.src = "./eye-close.png"
     }else if (password.type == "text"){
         password.type = "password"
-        eyeIcon.src = "./eye-close.png"
+        eyeIcon.src = "./eye-open.png"
     }
 }
